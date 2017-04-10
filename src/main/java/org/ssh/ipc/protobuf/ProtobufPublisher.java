068241fff825ab01fe6be5e6f4f51857968e93d0
Rimon Oz <rimooz@gmail.com>
2017-04-10 22:02:22 +0200

    fixes #6

--- src/main/java/org/ssh/ipc/protobuf/ProtobufPublisher.java
+++ src/main/java/org/ssh/ipc/protobuf/ProtobufPublisher.java
@@ -2,14 +2,12 @@ package org.ssh.ipc.protobuf;
 
 import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.Parser;
-import java.net.DatagramPacket;
 import java.net.InetAddress;
-import java.net.SocketException;
+import java.util.Arrays;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
-import org.ssh.ipc.ip.MulticastPublisher;
 import org.ssh.ipc.ip.NettyMulticastPublisher;
 import org.ssh.math.function.LambdaExceptions;
 import reactor.core.publisher.Flux;
@@ -20,14 +18,13 @@ import reactor.core.publisher.Flux;
  * This class describes a {@link Publisher} of {@link GeneratedMessage protobuf packets}
  * which receives protobuf packets from multicast and publishes them.
  *
- * @param <M> the type parameter
+ * @param <M> The type of GeneratedMessage.
  * @author Rimon Oz
  * @author Jeroen de Jong
  */
 @Slf4j
 @Value
 public class ProtobufPublisher<M extends GeneratedMessage> implements Publisher<M> {
-
   /**
    * The {@link com.google.protobuf.Parser protobuf parser}.
    */
@@ -43,13 +40,9 @@ public class ProtobufPublisher<M extends GeneratedMessage> implements Publisher<
 
   @Override
   public void subscribe(Subscriber<? super M> subscriber) {
-    try {
-      Flux.from(new NettyMulticastPublisher(this.getAddress(), this.getPort()))
-          .map(DatagramPacket::getData)
-          .map(LambdaExceptions.rethrowFunction(getParser()::parseFrom))
-          .subscribe(subscriber);
-    } catch (SocketException exception) {
-      log.error(exception.getMessage());
-    }
+    Flux.from(new NettyMulticastPublisher(this.getAddress(), this.getPort()))
+        .map(datagramPacket -> Arrays.copyOfRange(datagramPacket.getData(), 0, datagramPacket.getLength()))
+        .map(LambdaExceptions.rethrowFunction(getParser()::parseFrom))
+        .subscribe(subscriber);
   }
 }
