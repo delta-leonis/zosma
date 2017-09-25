cd076af91454d7fba529922292d23ed7e3f68207
Rimon Oz <rimooz@gmail.com>
2017-09-25 19:35:57 +0200

    added tap

--- src/main/java/org/ssh/game/engine/Deducer.java
+++ src/main/java/org/ssh/game/engine/Deducer.java
@@ -1,7 +1,9 @@
 package org.ssh.game.engine;
 
 import java.util.function.Function;
-import org.reactivestreams.Publisher;
+import lombok.Value;
+import org.reactivestreams.*;
+import reactor.core.publisher.*;
 
 /**
  * The Interface Deducer.
@@ -19,6 +21,26 @@ public interface Deducer<I, O> extends Function<Publisher<I>, Publisher<O>> {
     @Override
     public Publisher<I> apply(final Publisher<I> inputPublisher) {
       return inputPublisher;
+    }
+  }
+
+  /**
+   * The Class Tap.
+   *
+   * This class represents an intermediary {@link Subscriber}.
+   *
+   * @param <I> The type of input supplied to the {@link Tap}.
+   * @author Rimon Oz
+   */
+  @Value
+  class Tap<I> implements Deducer<I, I> {
+    private final Subscriber<I> subscriber;
+    @Override
+    public Publisher<I> apply(final Publisher<I> inputPublisher) {
+      final TopicProcessor<I> topicProcessor = TopicProcessor.create();
+      Flux.from(inputPublisher).subscribe(topicProcessor);
+      topicProcessor.subscribe(this.subscriber);
+      return topicProcessor;
     }
   }
 }
