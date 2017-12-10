673074838f1e8836c35a0e07dff5a1e159b75039
romnous <rimooz@gmail.com>
2017-12-10 22:50:01 +0100

    refactor FeedbackDeducer into GameSimulator (#24)

---
+++ src/main/java/io/leonis/zosma/game/engine/GameSimulator.java
@@ -0,0 +1,56 @@
+package io.leonis.zosma.game.engine;
+
+import org.reactivestreams.*;
+import reactor.core.publisher.*;
+
+/**
+ * The Interface GameSimulator.
+ *
+ * This interface describes the functionality of an game simulator. A game is simulated by acting on
+ * all received input, and publishing its initial state to itself, thus creating a feedback loop
+ * which can be used for simulating game start without an external game state processor.
+ *
+ * @param <I> The type of game state on which this simulator acts.
+ * @param <O> The type of strategic output produced by this simulator.
+ * @author Rimon Oz
+ */
+public interface GameSimulator<I, O> extends Deducer<I, O>, Publisher<O> {
+  @Override
+  default void subscribe(Subscriber<? super O> s) {
+    Flux.from(this.apply(
+        Flux.combineLatest(this.getInputProcessor(), this.getOutputProcessor(), this::project)
+            .startWith(this.getInitialInput())
+            .sampleMillis(this.getInputProcessorInterval())
+            .doOnNext(this.getInputProcessor()::onNext)));
+  }
+
+  /**
+   * @return The {@link TopicProcessor processor} which forwards outputs.
+   */
+  TopicProcessor<I> getInputProcessor();
+
+  /**
+   * @return The {@link TopicProcessor processor} which forwards outputs.
+   */
+  TopicProcessor<O> getOutputProcessor();
+
+  /**
+   * @return The initial input.
+   */
+  I getInitialInput();
+
+  /**
+   * @return The interval on which the {@link TopicProcessor game processor} publishes input.
+   */
+  long getInputProcessorInterval();
+
+  /**
+   * Projects an input state based on a previously projected input state and most recently produced
+   * output state.
+   *
+   * @param previousInput The previously projected input state.
+   * @param latestOutput  The latest output produced by the {@link ParallelDeducer}.
+   * @return The projected input state.
+   */
+  I project(final I previousInput, final O latestOutput);
+}
