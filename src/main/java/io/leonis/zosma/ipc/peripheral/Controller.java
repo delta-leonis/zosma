c99b0950447459e647e50a0bbf0a16b38c2368d1
romnous <rimooz@gmail.com>
2017-12-15 12:24:01 +0100

    Update Controller to conform to Identity.Supplier (#26)
    * update Controller to conform to Identity.Supplier
    
    * update Controller doc
    
    * update type parameter doc of Controller

--- src/main/java/io/leonis/zosma/ipc/peripheral/Controller.java
+++ src/main/java/io/leonis/zosma/ipc/peripheral/Controller.java
@@ -9,15 +9,15 @@ import java.util.*;
  * This interface describes the functionality of a controller which maps an entity's identifier
  * to the controls provided by the underlying implementation.
  *
- * @param <I> The type of identifier used to denote the controlled entity.
+ * @param <I> The type of identifier used to identify the controller
  * @param <S> The type of controls provided by the controller.
  * @author Jeroen de Jong
  */
-public interface Controller<I extends Identity, S> {
+public interface Controller<I extends Identity, S> extends Identity.Supplier {
   /**
-   * @return The identifier to which this controller links its controls.
+   * @return The identifier of the controller.
    */
-  I getIdentifier();
+  I getIdentity();
 
   /**
    * @return The controls provided by the controller.
@@ -26,7 +26,7 @@ public interface Controller<I extends Identity, S> {
 
   /**
    * Supplies a mapping of {@link Controller} to {@link Identity} of objects it is controlling.
-   * 
+   *
    * @param <C> The type of {@link Controller} being supplied.
    * @param <A> The type of {@link Identity} of the object it is controlling.
    */
