4d6281ccf3aa9845a89ff12debe43fa41d88b27b
Jeroen de Jong <thumbnail95@gmail.com>
2018-06-26 00:22:09 +0200

    Return triplet in VisionPacketAdapter

--- src/main/java/io/leonis/zosma/ipc/protobuf/VisionPacketAdapter.java
+++ src/main/java/io/leonis/zosma/ipc/protobuf/VisionPacketAdapter.java
@@ -1,44 +1,33 @@
 package io.leonis.zosma.ipc.protobuf;
 
-import io.leonis.algieba.Temporal;
 import io.leonis.zosma.game.data.*;
-import io.leonis.zosma.ipc.protobuf.VisionPacketAdapter.VisionPacket;
 import io.leonis.zosma.ipc.protobuf.vision.*;
+import io.leonis.zosma.parameters.Triplet;
 import io.reactivex.functions.*;
 import java.util.Set;
-import lombok.*;
-import lombok.experimental.Delegate;
+import lombok.AllArgsConstructor;
 import org.robocup.ssl.Wrapper.WrapperPacket;
 
 /**
  * The Class VisionPacketAdapter.
  *
- * This class represents a {@link Function} which creates {@link VisionPacket} from {@link
+ * This class represents a {@link Function} which creates {@link Triplet} from {@link
  * WrapperPacket}.
  *
  * @author Jeroen de Jong
  * @author Rimon Oz
  */
 @AllArgsConstructor
-public class VisionPacketAdapter implements BiFunction<WrapperPacket, AllegianceTuple<Team>, VisionPacket> {
+public class VisionPacketAdapter implements BiFunction<WrapperPacket, AllegianceTuple<Team>, Triplet<Field, Set<Ball>, AllegianceTuple<Set<Player>>>> {
 
   @Override
-  public VisionPacket apply(
+  public Triplet<Field, Set<Ball>, AllegianceTuple<Set<Player>>> apply(
       final WrapperPacket wrapperPacket,
       final AllegianceTuple<Team> teams
   ) throws Exception {
-    return new VisionPacket(
+    return new Triplet<>(
         new FieldAdapter().apply(wrapperPacket.getGeometry()),
         new BallsAdapter().apply(wrapperPacket.getDetection()),
         new PlayersAdapter().apply(wrapperPacket.getDetection(), teams));
-  }
-
-  @Value @AllArgsConstructor
-  public final static class VisionPacket implements Temporal {
-    @Delegate
-    private final Field field;
-    private final Set<Ball> balls;
-    private final AllegianceTuple<Set<Player>> players;
-    private final long timestamp = System.currentTimeMillis();
   }
 }
