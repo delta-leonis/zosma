a988d0a508b9bc906288fcaf35bada1a53678538
Jeroen de Jong <thumbnail95@gmail.com>
2018-06-30 18:29:03 +0200

    Refactor GoalAdapter to use SSL_Referee and Teams

--- src/main/java/io/leonis/zosma/ipc/protobuf/vision/GoalAdapter.java
+++ src/main/java/io/leonis/zosma/ipc/protobuf/vision/GoalAdapter.java
@@ -2,10 +2,12 @@ package io.leonis.zosma.ipc.protobuf.vision;
 
 import static io.leonis.zosma.game.data.Allegiance.*;
 import static io.leonis.zosma.game.data.FieldHalf.*;
+import static io.leonis.zosma.game.data.TeamColor.*;
 
 import io.leonis.zosma.game.data.*;
-import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function3;
 import org.robocup.ssl.Geometry.GeometryData;
+import org.robocup.ssl.Referee.SSL_Referee;
 
 /**
  * The Class GoalAdapter.
@@ -14,16 +16,25 @@ import org.robocup.ssl.Geometry.GeometryData;
  *
  * @author Jeroen de Jong
  */
-public class GoalAdapter implements BiFunction<GeometryData, Allegiance, AllegianceTuple<Goal>> {
+public class GoalAdapter implements
+    Function3<GeometryData, SSL_Referee, AllegianceTuple<Team>, AllegianceTuple<Goal>> {
+
   private final GoalDimensionAdapter goalDimensionAdapter = new GoalDimensionAdapter();
 
   @Override
-  public AllegianceTuple<Goal> apply(final GeometryData geometry, final Allegiance positiveTeamAllegiance) {
+  public AllegianceTuple<Goal> apply(
+      final GeometryData geometry,
+      final SSL_Referee referee,
+      final AllegianceTuple<Team> teams
+  ) {
     final GoalDimension dimension = goalDimensionAdapter.apply(geometry);
-    final FieldHalf allyFieldHalf = positiveTeamAllegiance.equals(ALLY) ? POSITIVE : NEGATIVE;
+    final TeamColor positiveTeamColor = referee.getBlueTeamOnPositiveHalf() ? BLUE : YELLOW;
+    final FieldHalf allyFieldHalf =
+        positiveTeamColor.equals(teams.getAlly().getTeamColor()) ? POSITIVE : NEGATIVE;
 
     return new AllegianceTuple<>(
         new Goal.State(dimension, allyFieldHalf, ALLY, geometry.getField().getFieldLength()),
-        new Goal.State(dimension, allyFieldHalf.opposite(), OPPONENT, geometry.getField().getFieldLength()));
+        new Goal.State(dimension, allyFieldHalf.opposite(), OPPONENT,
+            geometry.getField().getFieldLength()));
   }
 }
