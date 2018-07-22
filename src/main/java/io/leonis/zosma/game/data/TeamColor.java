6f6eef0a35c751ddd9f13e3edd0651a219dbdde8
Jeroen de Jong <thumbnail95@gmail.com>
2018-07-22 06:58:44 +0200

    Resolve final issues with javadoc

--- src/main/java/io/leonis/zosma/game/data/TeamColor.java
+++ src/main/java/io/leonis/zosma/game/data/TeamColor.java
@@ -3,7 +3,8 @@ package io.leonis.zosma.game.data;
 /**
  * The Enum TeamColor.
  *
- * Describes the color of a team. 
+ * Describes the color of a team. Note that most logic should be based on the {@link Allegiance} of
+ * a {@link Team} in order to take switching colors of teams into account.
  *
  * @author Jeroen de Jong
  */
@@ -11,7 +12,7 @@ public enum TeamColor {
   YELLOW, BLUE;
 
   /**
-   * @return Color of opponent team
+   * @return Color of opponent team.
    */
   public TeamColor opponent() {
     return this.equals(BLUE) ? YELLOW : BLUE;
